d05847dd2d10de357209025bf42df59a5a584146
xiangbin <lixiangbin@stonewise.cn>
2019-10-08 10:17:52 +0000

    Initial commit.

---
+++ build-projects.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# builds the top-level directory.
+mkdir $1 && cd $1 && \
+mkdir bin data results models src utils logs && \
+cd src && mkdir test && touch main.py && touch __init__.py && cd test && touch test_main.py && cd ../../ && \
+cd utils && touch __init__.py && cd ../ && \
+cd logs && touch logs.out
