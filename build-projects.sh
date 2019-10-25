593980cf79e6490cc083e01a2d5d408214f5b8ae
xiangbin <lixiangbin@stonewise.cn>
2019-10-25 03:45:45 +0000

    Fix bugs

--- build-projects.sh
+++ build-projects.sh
@@ -2,6 +2,6 @@
 # builds the top-level directory.
 mkdir $1 && cd $1 && \
 mkdir bin data results models src utils logs && \
-cd src && mkdir test && touch main.py && touch __init__.py && cd test && touch test_main.py && cd ../../ && \
+cd src && mkdir test && touch $1_main.py && touch __init__.py && cd test && touch test_main.py && cd ../../ && \
 cd utils && touch __init__.py && cd ../ && \
 cd logs && touch logs.out
